a9d611d9600592aedda051225de0e6bb8821b42a
anlunjiang <anlunjiang@kubrickgroup.com>
2019-09-17 00:36:06 +0100

    ws

--- 06_Operators/src/Operators.java
+++ 06_Operators/src/Operators.java
@@ -1,9 +1,19 @@
 public class Operators {
 
     public static void main(String[] args) {
+        // Operators perform operations - addition e.g.
+        // An operand is the object being manipulated by the operator
+        int x = 4 + 4; // equals is an assignment operator
+        System.out.printf("4 + 4 = %d", x);
 
-
-
-
+        x = x % 3;
+        System.out.println("\n" + x);
+        x ++; // x = x + 1
+        System.out.println("\n" + x);
+        x --; // x = x - 1
+        System.out.println("\n" + x);
+        x += 2; //x = x + 2
+        System.out.println("\n" + x);
+        // Can use for other operations *=, +=, -= etc
     }
 }
