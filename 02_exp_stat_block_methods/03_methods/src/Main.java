84f8308d39c85329fe9013388b6e82266d46ae04
anlunjiang <anlunjiang@kubrickgroup.com>
2019-09-22 13:39:17 +0100

    ws

--- 02_exp_stat_block_methods/03_methods/src/Main.java
+++ 02_exp_stat_block_methods/03_methods/src/Main.java
@@ -8,29 +8,33 @@ public class Main {
         Scanner input = new Scanner(System.in);
         int score = input.nextInt();
 
-        boolean gameOver = true;
+        boolean gameOver = false;
 
         int levelCompleted = 5;
         int bonus = 100;
 
-        if (gameOver) {
-            int finalScore = calculateScore(score, levelCompleted, bonus);
-
+        if (score > 0) {
+            int finalScore = calculateScore(gameOver, score, levelCompleted, bonus);
             System.out.printf("\nOverall you completed %d Levels" +
                     "\nwith a bonus of %d per level" +
                     "\nTotal Score of %d", levelCompleted, bonus, finalScore);
-
         } else {
-            System.out.println("The game has not finished yet");
+            System.out.println("You have not entered a valid score, exiting");
+            System.exit(0);
         }
 
     }
 
     // New method outside of main method but in the class
-    public static int calculateScore(int score, int levelCompleted, int bonus) {
+    public static int calculateScore(boolean gameOver, int score,
+                                     int levelCompleted, int bonus) {
+        if (gameOver) {
+            return score + (levelCompleted * bonus);
+        } else {
+            System.out.println("The game has not finished yet");
+            System.exit(0);
+            return -1;
+        }
 
-        int finalScore = score + (levelCompleted * bonus);
-
-        return finalScore;
     }
 }
